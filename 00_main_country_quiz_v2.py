0a64dacde2cc0b338af30c5fb35d3b094476a723
daenaram <129803732+daenaram@users.noreply.github.com>
2023-10-29 00:27:59 +1300

    Add files via upload

---
+++ 00_main_country_quiz_v2.py
@@ -0,0 +1,380 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+import random
+import csv
+
+
+class Start:
+
+    def __init__(self):
+
+        self.feedback = StringVar()
+        self.feedback.set("")
+
+        self.feedback_error = StringVar()
+        self.feedback_error.set("no")
+
+        # start GUI frame
+        self.quiz_frame = Frame()
+        self.quiz_frame.grid()
+
+        self.quiz_heading = Label(self.quiz_frame,
+                                  text="Country Capitals",
+                                  font=("Arial", "16", "bold"))
+        self.quiz_heading.grid(row=0)
+
+        instruction = "Please enter how many questions you would like to answer and press enter."
+        self.quiz_instruction = Label(self.quiz_frame,
+                                      text=instruction,
+                                      wraplength=260, width=60)
+        self.quiz_instruction.grid(row=1)
+
+        # Frame for the textbox and enter button
+        self.input_frame = Frame(self.quiz_frame)
+        self.input_frame.grid(row=2)
+
+        self.quiz_entry = Entry(self.input_frame,
+                                font=("Arial", "14"))
+        self.quiz_entry.grid(row=0, column=0, padx=5, pady=5)
+
+        self.enter_button = Button(self.input_frame, text="Enter",
+                                   bg="#a2eba2", fg="#000000",
+                                   font=("Arial", "14", "bold"),
+                                   padx=10, pady=1,
+                                   command=self.to_enter)
+        self.enter_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.error_label = Label(self.quiz_frame, text="",
+                                 font=("Arial", "10"),
+                                 fg="#9C0000")
+        self.error_label.grid(row=3)
+
+    # error message and the text box turns red
+    # only accepts whole numbers
+    def check_rounds(self, min_value, max_value):
+
+        has_error = "no"
+        error = "Please enter a number more than {} to {}".format(min_value, max_value)
+
+        response = self.quiz_entry.get()
+
+        try:
+            response = int(response)
+
+            if response < min_value:
+                has_error = "yes"
+
+            elif response > max_value:
+                has_error = "yes"
+
+        except ValueError:
+            has_error = "yes"
+
+        if has_error == "yes":
+            self.feedback_error.set("yes")
+            self.feedback.set(error)
+            return "invalid"
+
+        else:
+            self.feedback_error.set("no")
+            self.to_game(response)
+
+    # checks if the number of question entered is
+    # from 1 to 20
+    def to_enter(self):
+        to_play = self.check_rounds(1, 20)
+
+        if to_play != "invalid":
+            self.feedback.set("".format(to_play))
+
+        self.output_outcome()
+
+    def output_outcome(self):
+        output = self.feedback.get()
+        has_error = self.feedback_error.get()
+
+        if has_error == "yes":
+            self.error_label.config(fg="#9C0000")
+            self.quiz_entry.config(bg="#F8CECC")
+
+        else:
+            self.error_label.config(fg="#004C00")
+            self.quiz_entry.config(bg="#FFFFFF")
+
+        self.error_label.config(text=output)
+
+    def to_game(self, num_rounds):
+        Game(num_rounds)
+
+        root.withdraw()
+
+
+# Quiz GUI
+class Game:
+
+    def __init__(self, how_many):
+        self.game_box = Toplevel()
+
+        self.game_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_game))
+
+        # store the capitals for the options
+        self.capitals_variable = StringVar()
+        self.capitals_variable.set("")
+
+        # Variables used to work out statistics, when game ends etc
+        self.questions_wanted = IntVar()
+        self.questions_wanted.set(how_many)
+
+        self.questions_played = IntVar()
+        self.questions_played.set(0)
+
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        number_game_question = "Choose - Round 1 of {}".format(how_many)
+        self.choose_heading = Label(self.game_frame,
+                                    text=number_game_question,
+                                    font=("Arial", "14", "bold"))
+        self.choose_heading.grid(row=0)
+
+        # store the csv file for the questions and options
+        self.button_options = self.get_csv_file()
+        self.button_list_options = self.get_quest_ans()
+
+        self.quiz_list = []
+
+        self.game_question = Label(self.game_frame,
+                                   text="",
+                                   wraplength=260, width=60)
+
+        self.game_question.grid(row=1)
+
+        # option frame
+        self.options_frame = Frame(self.game_frame)
+        self.options_frame.grid(row=2)
+
+        self.options_button_ref = []
+
+        for item in range(0, 4):
+            self.options_button = Button(self.options_frame,
+                                         fg="#000000",
+                                         bg="#A3D4FF",
+                                         width=30, height=2,
+                                         command=lambda item=item: self.right_wrong_ans(self.quiz_list[item][1]))
+
+            self.options_button_ref.append(self.options_button)
+
+            self.options_button.grid(row=item // 2,
+                                     column=item % 2,
+                                     padx=5, pady=5)
+
+        # random.shuffle(self.options_button_ref)
+
+        # control button frame
+        self.control_frame = Frame(self.game_frame)
+        self.control_frame.grid(row=3)
+
+        control_buttons = [
+            ["#B1DDF0", "Next Question", "next question"],
+            ["#FFE6CC", "Help", "get help"],
+            ["#DAE8FC", "History", "get history"],
+            ["#FAD9D5", "Start Over", "start over"]
+        ]
+
+        self.control_btn_ref = []
+
+        for item in range(0, 4):
+            self.make_control_btn = Button(self.control_frame,
+                                           bg=control_buttons[item][0],
+                                           text=control_buttons[item][1], width=12,
+                                           font=("Arial", "12", "bold"),
+                                           command=lambda i=item: self.to_do(control_buttons[i][2]))
+            self.make_control_btn.grid(row=0, column=item, padx=5, pady=5)
+
+            # add buttons to control list
+            self.control_btn_ref.append(self.make_control_btn)
+
+        # to enable and disabled next question button
+        self.to_next_question = self.control_btn_ref[0]
+        self.to_help_btn = self.control_btn_ref[1]
+
+        # at starts, get 'new round'
+        self.next_question()
+
+    # the button action lists
+    def to_do(self, action):
+        if action == "get help":
+            Help(self)
+        elif action == "get history":
+            print("History")
+        elif action == "start over":
+            self.close_game()
+        else:
+            self.next_question()
+
+    # getting the csv file
+    def get_csv_file(self):
+
+        self.capitals_variable.get()
+
+        file = open("country_capitals.csv", "r")
+        var_all_questions = list(csv.reader(file, delimiter=","))
+        file.close()
+
+        # removes the first row in the csv file
+        var_all_questions.pop(0)
+
+        return var_all_questions
+
+    def get_quest_ans(self):
+        options = []
+
+        while len(options) < 4:
+            choose = random.choice(self.button_options)
+            index_chosen = self.button_options.index(choose)
+
+            # only adds item if it's not already in the list
+            if choose not in options:
+                options.append(choose)
+
+                # remove item from master list
+                self.button_options.pop(index_chosen)
+
+        return options
+
+    def next_question(self):
+        # Reset the backgrounds of all option buttons to #A3D4FF
+        for item in self.options_button_ref:
+            item.config(bg="#A3D4FF")
+
+        # disables next question every round
+        # before the user chooses an option
+        self.to_next_question.config(state=DISABLED)
+
+        # empty button list so we can get new capitals
+        self.quiz_list.clear()
+
+        # get new capitals for buttons
+        self.quiz_list = self.get_quest_ans()
+
+        # the question every round
+        # retrieve correct answer!!
+        question = self.quiz_list[0][0]
+        self.correct_answer = self.quiz_list[0][1]  # Store the correct answer
+        # print("correct answer is", self.correct_answer)
+
+        # shuffle list here
+        random.shuffle(self.quiz_list)
+
+        # Construct the full question text
+        full_question = f"What is the capital of {question}"
+        self.game_question.config(text=full_question)
+
+        # Set the text and state for each button
+        for idx, item in enumerate(self.options_button_ref):
+            item['text'] = self.quiz_list[idx][1]
+            item['state'] = NORMAL
+
+        # retrieve number of questions wanted / played
+        # and update heading.
+        how_many = self.questions_wanted.get()
+        current_round = self.questions_played.get()
+        new_heading = "Question {} of {}".format(current_round + 1, how_many)
+        self.choose_heading.config(text=new_heading)
+
+    def right_wrong_ans(self, selected_option_text):
+        selected_option_index = ""
+        # print("you choose", selected_option_text)
+
+        how_many = self.questions_wanted.get()
+
+        # add one to number of question played
+        current_question = self.questions_played.get()
+        current_question += 1
+        self.questions_played.set(current_question)
+
+        # deactivate option buttons to prevent the user
+        # to choose another option
+        for item in self.options_button_ref:
+            item.config(state=DISABLED)
+
+        # disable the next button once they finished all the
+        # questions wanted
+        if current_question < how_many:
+            self.to_next_question.config(state=NORMAL)
+
+        right = "#D5E8D4"
+        wrong = "#F8CECC"
+
+        if selected_option_text == self.correct_answer:  # Compare with the correct answer
+            for idx, option in enumerate(self.quiz_list):
+                if option[1] == selected_option_text:
+                    selected_option_index = idx
+                    break
+            # print("selected button index", selected_option_index)
+            self.options_button_ref[selected_option_index].config(bg=right)
+        else:
+            for idx, option in enumerate(self.quiz_list):
+                if option[1] == selected_option_text:
+                    selected_option_index = idx
+                    break
+            # print("selected button index", selected_option_index)
+            self.options_button_ref[selected_option_index].config(bg=wrong)
+
+    # starts the game over
+    def close_game(self):
+        root.deiconify()
+        self.game_box.destroy()
+
+
+# help button
+class Help:
+    def __init__(self, partner):
+        # dialogue box
+        self.help_box = Toplevel()
+
+        # display help button
+        partner.to_help_btn.config(state=DISABLED)
+
+        self.help_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_help, partner))
+
+        self.help_frame = Frame(self.help_box, width=300,
+                                height=200, bg="#FFE6CC")
+        self.help_frame.grid()
+
+        self.help_heading_label = Label(self.help_frame, bg="#FFE6CC",
+                                        text="Help / Hints",
+                                        font=("Arial", "14", "bold"))
+        self.help_heading_label.grid(row=0, pady=5)
+
+        help_instruction = "This quiz will test your knowledge of countries and their capitals. " \
+                           "You'll be presented with a series of questions about different countries. " \
+                           "Choose the correct capital for each country from the provided options. \n\n" \
+                           "Good luck and enjoy the quiz!"
+
+        self.help_text_label = Label(self.help_frame, bg="#FFE6CC",
+                                     text=help_instruction, wraplength=350,
+                                     justify="left")
+        self.help_text_label.grid(row=1, padx=10, pady=5)
+
+        self.dismiss_button = Button(self.help_frame,
+                                     font=("Arial", "12", "bold"),
+                                     text="Dismiss", bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     command=partial(self.close_help,
+                                                     partner))
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+    def close_help(self, partner):
+        partner.to_help_btn.config(state=NORMAL)
+        self.help_box.destroy()
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Country Quiz")
+    Start()
+    root.mainloop()
